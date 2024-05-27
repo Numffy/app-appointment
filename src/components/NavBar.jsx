7912a379355ac949a36d897986d45124a3e33c84
numffy <franco76297124@gmail.com>
2024-05-26 20:41:21 -0500

    BBBBBBBBB

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,9 +2,9 @@ import { getServerSession } from "next-auth";
 import { handler } from "../app/api/auth/[...nextauth]/route";
 import ClientNavbar from '../components/ClientNavbar';
 
-async function NavBar() {
+async function Navbar() {
   const session = await getServerSession(handler);
   return <ClientNavbar session={session} />;
 }
 
-export default NavBar;
+export default Navbar;
